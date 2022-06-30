a561dd85e80f4717ce7516570976ceedc49dbf8c
tm <mykolyk@stuycs.org>
2022-06-30 14:03:15 -0400

    + test code for what is in a new array

---
+++ daily/1/TestArrayInit.java
@@ -0,0 +1,11 @@
+public class TestArrayInit {
+
+  public static void main( String[] args ) {
+    int theArray[];
+    theArray = new int[17];
+
+    for( int i=0; i<theArray.length; i++) {
+      System.out.println(theArray[i]);
+    }
+  }
+}
