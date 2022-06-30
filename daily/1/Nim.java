97d4104dddd35107073ae77fa09178a2a3d8d804
tm <mykolyk@stuycs.org>
2022-06-30 09:57:34 -0400

    +Nim skeleton

---
+++ daily/1/Nim.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Nim 
+{
+
+  public static void main( String[] args ) 
+{
+
+    int stones = 12;
+    int stonesTaken;
+
+    Scanner input = new Scanner(System.in);
+
+    //loop until game ends
+    while (  ) {
+      //prompt for user input : num of stones
+
+      //calculate number of stones remaining, print
+
+      //check for win
+
+      //machine turn
+
+      //check for win
+
+      //calculate number of stones remaining, print
+
+      //check for win
+    }
+
+
+  }
+
+
+}
