7a7ae38c6a36228de69622697bb2e5fc7733f181
tm <mykolyk@stuycs.org>
2022-06-30 19:22:39 -0400

    slight edits for clarity

--- daily/2/ArrayPractice.java
+++ daily/2/ArrayPractice.java
@@ -1,10 +1,9 @@
 import java.util.*;
 import java.io.*;
 
-/* Instructions
-
-   This file contains stubs (empty methods) for the following methods
-
+/**
+   INSTRUCTIONS:
+   This file contains stubs (empty methods) for the following methods:
 
    1. buildIncreasingArray
    2. buildRandomArray
@@ -19,13 +18,14 @@ import java.io.*;
    The stubs will have comments describing what they should do
 
    Levels:
+   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    Basic:
    Complete these methods:
-     - buildRandomArray
-     - printArray
-     - arraySum
-     - firstOccurence 
-     - findMaxValue
+   - buildRandomArray
+   - printArray
+   - arraySum
+   - firstOccurence
+   - findMaxValue
 
    Intermediate:
    Complete all the methods for basic and also
@@ -35,177 +35,206 @@ import java.io.*;
 
    Advanced:
    Complete all the methods
-   
-   
 */
 
-public class ArrayPractice{
+public class ArrayPractice
+{
 
-    /*
-      Parameters: size - how large the array should be
-                  startValue - the value of the first element
-		  step - the interval to the next value
-      Returns: a new array of integers
+  /**
+     Parameters:
+     size - how large the array should be
+     startValue - the value of the first element
+     step - the interval to the next value
 
-      Examples:
-      buildIncreasingArray(5,3,2) will return an array
-      with the values 3,5,7,9,11 (five values, starting with 3 with a step of 2)
-      
-    */
-    public static int[] buildIncreasingArray(int size, int startValue, int step){
-	int[] data = new int[size];
+     Returns:
+     a new array of integers
 
-	// loop through each element of the array and
-	// assign the appropriate value to each one.
+     Examples:
+     buildIncreasingArray(5,3,2) will return an array
+     with the values 3,5,7,9,11 (five values, starting with 3 with a step size of 2)
+  */
+  public static int[] buildIncreasingArray( int size, int startValue, int step )
+  {
+    int[] data = new int[size];
 
-	return data;
-    }
+    // loop through each element of the array and
+    /* YOUR BRILLIANT CODE HERE */
 
-    /* Parameters: size - how many elements in the array
-                   maxValue - the largest possible random value to use
+    // assign the appropriate value to each one.
+    /* YOUR BRILLIANT CODE HERE */
 
-       Returns: a new array where each element is an integer between 0
-       and up to but not including maxValue
-
-    */
-    public static int[] buildRandomArray(int size, int maxValue){
-	Random r = new Random();
-	int[] data = new int[size];
-
-	// your code here
-
-	return data;
-    }
+    return data;
+  }
 
 
-    /* Parameters: data - ana rray of ints 
+  /**
+     Parameters:
+     size - how many elements in the array
+     maxValue - the largest possible random value to use
 
-       Returns: nothing
+     Returns:
+     a new array where each element is an integer between 0
+     and up to but not including maxValue
+  */
+  public static int[] buildRandomArray( int size, int maxValue )
+  {
+    Random r = new Random();
+    int[] data = new int[size];
 
-       prints out the array
+    /* YOUR BRILLIANT CODE HERE */
 
-       Note: data.length stores the length of the array
-
-    */
-    public static void printArray(int[] data){
-    }
-    
-
-
-    /* Parameters: data - an array of integers
-                   value - the value to search for
-		   
-       Returns: the index (location) of the first occurence of value
-
-       Example: assume the array data has 1,5,2,7,5,8,5,12,19,5
-       firstOccurence(data,5) would return 1 since the first
-       occurence of the value 5 is at a[1]
-    */
-    public static int firstOccurence(int[] data, int value){
-
-	// your code here
-	
-	return 0; // change this
-    }
-    
-    
-    
-    /* Parameters: data - an array of ints
-
-       Returns: an integer that is equal to the sum of all the elements in the array
-
-       Ex: If the input array has the values 5,2,4,10 the return
-       value will be 21 (5+2+4+10)
-
-    */
-    public static int arraySum(int[] data){
-
-	// your code here
-	
-	return 0; // replace this
-    }
+    return data;
+  }
 
 
-    /* Parameters: data - an array of integers
+  /**
+     Parameters:
+     data - an array of ints
 
-       Returns: true if the array is sorted, false otherwise
+     Returns:
+     nothing
 
-       That is if every element is < the element to it's right the array is
-       sorted.
+     prints out the array
 
-       An array with values 5,6,10,15 is sorted and one with values
-       5,6,10,13,15 isn't
-
-    */
-    public static boolean isSorted(int[] data){
-
-	// yourcode here
-	
-	return true; // replace this 
-	
-    }
-
-    /* Parameters: data - an array of integers
-
-       Returns: the value of the largest element in the array
-
-    */
-    public static int findMaxValue(int[] data){
-	int m; // will hold the maximum value;
-
-	// your code here
-	
-	return m;
-    }
+     Note: data.length stores the length of the array
+  */
+  public static void printArray( int[] data )
+  {
+    /* YOUR BRILLIANT CODE HERE */
+  }
 
 
-    /* Parameters: data - an array of integers
 
-       Returns: the number of odd elements in the array
+  /**
+     Parameters:
+     data - an array of integers
+     value - the value to search for
 
-       Ex: if data holds 5,6,7,8,9,10 the return value will be 3 since
-       three of the elements are odd.
+     Returns:
+     the index (location) of the first occurence of value
 
-    */
-    public static int countOdds(int[] data){
-	int count;
+     Example:
+     given array data, containing 1,5,2,7,5,8,5,12,19,5
+     firstOccurence(data,5) would return 1
+     since the first occurence of the value 5 is at a[1]
+  */
+  public static int firstOccurence( int[] data, int value )
+  {
 
-	// your code here
-	// note the % operator is the mod (basically remainder) function
-	// in java, You can use that to determine if an integer is odd
+    /* YOUR BRILLIANT CODE HERE */
 
-	return count;
-    }
+    return 0; // change this (it is just a placeholder to "get past" the compiler)
+  }
 
-    /* Parameters: data - an array of integers
 
-       Returns: no return value since this is a void function but you
-       will manipulate the values in the array parameter
+  /**
+     Parameters: data - an array of ints
 
-       This routine will flip all the elements in the array, that is,
-       swap all the values.
+     Returns: an integer that is equal to the sum of all the elements in the array
 
-       If the input array contains 5,8,13,7,27,2
-       After the routine, the array will contain 2,27,7,13,8,5
+     Ex: If the input array has the values 5,2,4,10 the return
+     value will be 21 (5+2+4+10)
 
-    */
-    public static void flip(int[] data){
+  */
+  public static int arraySum( int[] data )
+  {
+    /* YOUR BRILLIANT CODE HERE */
 
-	// your code here
-    }
-    
-    
+    return 0; // replace this
+  }
 
-    public static void main(String[] args) {
 
-	// remove the comments as you complete each routine
-	// and add more lines as you add additional routines.
-	
-	// int[] data = buildRandomArray(10, 20);
-	// int[] data2 = buildincreasingArray(10,5,3);
-	// printArray(data);
-	// printArray(data2);
+  /**
+     Parameters:
+     data - an array of integers
 
-	// add calls to show that the methods you write work.
-    }
+     Returns:
+     true if the array is sorted, false otherwise
+
+     That is, if each element is < the element to its right
+     then the array is sorted.
+
+     An array with values 5,6,10,15 is sorted
+     An array with values 5,6,10,13,15 is not
+
+  */
+  public static boolean isSorted( int[] data )
+  {
+    /* YOUR BRILLIANT CODE HERE */
+
+    return true; // replace this
+
+  }
+
+  /**
+     Parameters:
+     data - an array of integers
+
+     Returns:
+     value of the largest element in the array
+  */
+  public static int findMaxValue( int[] data ) {
+    int m;  // will hold the maximum value;
+
+    /* YOUR BRILLIANT CODE HERE */
+
+    return m;
+  }
+
+
+  /**
+     Parameters:
+     data - an array of integers
+
+     Returns:
+     the number of odd elements in the array
+
+     Ex: if data holds 5,6,7,8,9,10 then the return value
+     will be 3 since three of the elements are odd.
+  */
+  public static int countOdds( int[] data ) {
+    int count;
+
+    /* YOUR BRILLIANT CODE HERE */
+
+    // Note the % operator is the modulo (basically remainder) function
+    // in java. Use to determine if an integer is odd.
+
+    return count;
+  }
+
+  /**
+     Parameters:
+     data - an array of integers
+
+     Returns:
+     no return value since this is a void function -- but
+     you will manipulate the values in the array parameter
+
+     Postcondition:
+     elements of the input array are in reverse order
+
+     Example:
+     If the input array contains 5,8,13,7,27,2
+     After the routine, the array will contain 2,27,7,13,8,5
+  */
+  public static void flip( int[] data )
+  {
+    /* YOUR BRILLIANT CODE HERE */
+  }
+
+
+  public static void main( String[] args )
+  {
+
+    // remove the comments as you complete each routine
+    // and add more lines as you add additional routines.
+
+    // int[] data = buildRandomArray(10, 20);
+    // int[] data2 = buildincreasingArray(10,5,3);
+    // printArray(data);
+    // printArray(data2);
+
+    // add calls to show that the methods you write work.
+  }
 }
