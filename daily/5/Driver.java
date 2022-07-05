8aa28d83d224858848ab2812a4ee7d4a08db598c
Mike Zamansky <zamansky@gmail.com>
2022-07-05 11:18:12 -0400

    updated Time.java Driver.java - need to update Readme

--- daily/5/Driver.java
+++ daily/5/Driver.java
@@ -1,15 +1,34 @@
 import java.io.*;
 import java.util.*;
 
+
+/**
+ * Time class by Team LucidThinkeren
+ * First Last
+ * collaborators: First Last, First Last
+ */
+
+/**
+   INSTRUCTIONS:
+
+   This file contains the Driver starter code for our Time class.
+   
+   Add code in main to test all your methods
+   
+   Place this file in a folder named programming/5/Driver.java
+
+*/
+
+
 public class Driver {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    //declare a var of appropriate type to assign an instance of Time to
-    Time t;
+	//declare a var of appropriate type to assign an instance of Time to
+	Time t;
 
-    //assign var the address of a newly-apportioned Time object
-    t = new Time();
-  }//end main()
+	//assign var the address of a newly-apportioned Time object
+	t = new Time();
+    }//end main()
 
 }//end class
--- daily/5/Time.java
+++ daily/5/Time.java
@@ -1,6 +1,41 @@
 import java.io.*;
 import java.util.*;
 
+
+/**
+ * Time class by Team LucidThinkeren
+ * First Last
+ * collaborators: First Last, First Last
+ */
+
+/**
+   INSTRUCTIONS:
+
+   This file contains the starter code for our Time class.
+
+   The class here uses ints for hours, minutes, and seconds but you
+   could decide to change the internal representation to just store
+   an int representing a number of seconds.
+
+   Place this file in a folder named programming/5/Time.java
+
+   Basic level (complete all):
+   - public Time(int hrs, int mins, int secs) - constructor
+   - public void toString()
+   - public void set(int hrs, int mins, int secs)
+
+   Intermediate level (complete Basic methods plus this method):
+   - public void add(Time other)
+   - public boolean isEquals(Time other)
+
+   
+   Advanced level (complete Basic + Intermediate + these two methods):
+   - public int compareTo(Time other)
+   
+*/
+
+
+
 public class Time {
     // Instance Variable(s)
     // You can change this if you want to use the alternate
